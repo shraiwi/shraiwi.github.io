c0a58df4b244c9951fd83ebf50a69b1f8869ac07
shraiwi <40672030+shraiwi@users.noreply.github.com>
2024-02-27 21:02:27 +1300

    Major cleanup

--- convert.sh
+++
@@ -1,8 +0,0 @@
-
-export DIR=md
-export FILES=$(ls -1 "$DIR" | sed -e 's/\.md$//')
-
-while IFS= read -r NAME; do
-	echo "$NAME.md -> $NAME.html"
-	pandoc -s --template "template.html" -o "$NAME.html" "$DIR/$NAME.md"
-done <<< "$FILES"
